a43a1d2bd7243151bd3a2aff2f2975a1fbd86e67
chihiro-a <chihiro.i.0128@gmail.com>
2021-07-18 08:29:07 +0000

    [add]oddSum

--- hello.java
+++ hello.java
@@ -17,5 +17,10 @@ public class hello {
     for(String name:names){
       System.out.println("私の名前は" + name + "です");
     }
+    
+    int[] numbers = {1, 4, 6, 9, 13, 16};
+    int oddSum = 0;
+    int evenSum = 0;
+    
   }
 }
